7fa47ca92b9a480153fc47d5cf1e2f528fe33720
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-06-23 01:19:06 +0200

    implement function to upload fine tune jsonl to openai

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 require("dotenv/config");
 
 const { readFile, writeFile, rm } = require("node:fs/promises");
-const { existsSync } = require("node:fs");
+const { existsSync, createReadStream } = require("node:fs");
 const path = require("node:path");
 
 const { OpenAIApi, Configuration } = require("openai");
 
 const openai = new OpenAIApi(new Configuration({
-  organization: process.env.ORGANIZATION,
+  organization: process.env.ORGANIZATION, // https://platform.openai.com/account/org-settings
   apiKey: process.env.OPENAI_KEY
 }));
+
+const compiledJSONPath = path.join(__dirname, "compiled.jsonl");
+const fineTuneFileIDPath = path.join(__dirname, "fineTuneFileID");
 
 // 1. compile .txt to .jsonl
 // yet im still trying to find an efficient way turning paragraph into one liner
 async function compileToJSONL() {
   const endOfLine = "\n";
   const rawConversationFilePath = path.join(__dirname, "conversation.txt");
-  const compiledJSONPath = path.join(__dirname, "compiled.jsonl");
   
   if (existsSync(compiledJSONPath)) {
     await rm(compiledJSONPath, { force: true, recursive: true });
@@ -43,6 +45,22 @@ async function compileToJSONL() {
   });
 };
 
+// 2. upload file to openai
+async function uploadFileForFineTuning() {
+  try {
+    const response = await openai.createFile(createReadStream(compiledJSONPath), 'fine-tune');
+    
+    console.log(response.data);
+
+    await writeFile(fineTuneFileIDPath, response.data.id);
+  } catch (error) {
+    console.error(error);
+  };
+
+  return;
+};
+
 module.exports = {
-  compileToJSONL
+  compileToJSONL,
+  uploadFileForFineTuning
 };
